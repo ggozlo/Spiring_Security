186c5f318f3e75facaceaf244e3033499bd81143
ggozlo <ggozlo@naver.com>
2021-06-25 03:25:33 +0900

    인증, 인증저장소

--- basicsecurity/src/main/java/security/basicsecurity/Configure/SecurityConfig.java
+++ basicsecurity/src/main/java/security/basicsecurity/Configure/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 
 @Configuration
@@ -14,6 +16,29 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 // 다수의 시큐리티 설정 클래스가 있다면 순서를 지정해야함, Top-Down 방식으로 FilterChainProxy 클래스가 참조 하므로
 // 매칭되는 url 정보가 넓고 포괄적일수록 후순위로 지정해야 한다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    /*
+    * Authentication (인증, 인증주체) : 자신이 누구인이 증명하는 것
+    * 사용자의 인증 정보를 저장하는 토큰 개념
+    * 인증 시 id, pw를 담고 인증 검증을 위해 전달되어 사용
+    * 인증후 인증결과 ( 사용자 객체, 권한 ) 를 담아서 SecurityContext 에 저장되어 전역적 참조가 가능
+    *  Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    *  전역적으로 인증 결과 참조, 사용 가능
+    * Authentication 객체 : principal(사용자 아이디 또는 사용자 객체, Object), credentials(사용자 패스워드),
+    * authorities(인증된 사용자 권한 목록), details(인증 부가 정보), authenticated(인증 여부부
+    *
+    *  SecurityContext
+    *  TheadLocal  > SecurityContext > Authentication > User
+    * TheadLocal : 각 쓰레드에 할당된 저장소, 다른 쓰레드와 공유 x , 아무곳에서나 참조가 가능함 (전역적으로 존재한다)
+    * 인증 완료시 HttpSession 에 저장되어 어플리케이션 전반에서 참조 가능
+    *
+    * SecurityContextHolder
+    * SecurityContext 객체의 저장 방식을 설정 할수 있게 함, 래핑하는듯?
+    * 전약은 스레드당 하나(기본값), 자식 스레드에 동일한 컨텍스트를 유지하는 상속전략
+    *  static 으로 응용 프로그램 전역에서 하나만 저장하는 전략.
+    * SecurityContextHolder.clearContext() 로 기존 정보 초기화 가능 - SecurityContext, Authentication 초기화됨
+    * */
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -25,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .httpBasic()
         ;
+
     }
 }
 
@@ -37,8 +63,11 @@ class SecurityConfig2 extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .anyRequest()
-                .permitAll()
+                .authenticated()
                 .and()
                 .formLogin();
+
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+        // 시큐리티 컨텍스트 홀더의 전략 설정도 웹보안설정어댑터 를 상소받은 설정클래스의 설정 메서드에서 설정한다
     }
 }
--- basicsecurity/src/main/java/security/basicsecurity/controller/SecurityController.java
+++ basicsecurity/src/main/java/security/basicsecurity/controller/SecurityController.java
@@ -1,10 +1,49 @@
 package security.basicsecurity.controller;
 
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
+    @GetMapping("/")
+    public String index(HttpSession session) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 시큐리티 홀더에서 컨텍스트 를 받아서 인증된 계정을 받아올수 있다
+        System.out.println("authentication.getName() = " + authentication.getName());
+        SecurityContext context = (SecurityContext) session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
+        // 인증이 완료되면  세션에도 저장이 되기 때문에 세션에서 컨텍스트를 받아와서 계정을 꺼낼수도 있다.
+        System.out.println("authentication1.getName() = " + authentication1.getName());
+
+        return "home";
+    }
+
+    @GetMapping("/thread")
+    public String thread() {
+
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                        System.out.println("authentication.getName() = " + authentication.getName());
+                        // 자식 스레드를 생성하여 시큐리티 컨텍스트의 인증 정보에서 계정명을 출력 하였으나
+                        // 스레드 로컬 저장 방식에서는 null 이 나온다 ( 객체 공유 불가 )
+                        // 상속 스레드 로컬 전략에서는 자식 스레드와 부모 스레드간의 공유가 된다
+                    }
+                }
+        ).start();
+
+        return "thread";
+    }
 
 }
